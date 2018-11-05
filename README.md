5d200a76f787638f2f3254f0d3c09705e2eaf05b
j-n4m4573 <jg_220@yahoo.com>
2018-11-05 12:37:14 -0800

    added changes

---
+++ README.md
@@ -0,0 +1,20 @@
+**Version 1.0.0**
+
+## Description
+<Description of project>
+<Why this project exists>
+
+
+## Run the app
+
+
+## TO DO (Features, Add-ons, Design)
+
+## Bugs
+
+## Contributors
+- Jamar Gibbs
+- Nathan Pillai natepill@gmail.com
+
+## License & copyright
+© Nathan Pillai, Jamar Gibbs --InvestorDash
---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+const express = require('express')
+const app = express()
+
+
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+
+
+app.get('/', (req, res) => {
+    res.render('home', {msg:'Check out our other properties'});
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+})
