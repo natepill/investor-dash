74a823f90495f8c6fd45424fd5b53b2c5f768eb3
Nathan Pillai <natepill@gmail.com>
2018-11-05 21:53:44 -0800

    Heroku scripts, env ports, heroku debugging

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 
-
 const express = require('express')
 const app = express()
-
-
 var exphbs = require('express-handlebars');
+
+//Uncomment when we have mongo added
+// var mongoose = require('mongoose');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/<name of mongodb>');
+
+const port = process.env.PORT || 3000;
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -16,6 +19,6 @@ app.get('/', (req, res) => {
     res.render('home', {msg:'Check out our other properties'});
 })
 
-app.listen(3000, () => {
-    console.log('listening on port 3000')
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
 })
